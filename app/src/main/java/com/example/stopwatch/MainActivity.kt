f054478b176996aeead869b32f69447d5290e22b
Daxton Brown <nugnuts.the.2nd@gmail.com>
2025-09-17 09:20:45 -0700

    start/stop and reset buttons

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.stopwatch
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var timer: Chronometer
+    private lateinit var buttonStartStop: Button
+    private lateinit var buttonReset: Button
+    private var isRunning: Boolean = false
+    private var pauseTime: Long = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +27,36 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         Log.d("MainActivity", "onCreate: ")
+        wireWidgets()
+
+        buttonStartStop.setOnClickListener {
+            if (!isRunning) {
+                isRunning = true
+                buttonStartStop.text = "Stop"
+                if (pauseTime != 0L) timer.base = pauseTime
+                timer.start()
+            }
+            else {
+                isRunning = false
+                buttonStartStop.text = "Start"
+                timer.stop()
+                pauseTime = timer.base
+            }
+        }
+
+        buttonReset.setOnClickListener {
+            isRunning = false
+            buttonStartStop.text = "Start"
+            timer.stop()
+            timer.base = SystemClock.elapsedRealtime()
+            pauseTime = timer.base
+        }
+    }
+
+    private fun wireWidgets() {
+        timer = findViewById(R.id.chronometer_main_stopwatch)
+        buttonStartStop = findViewById(R.id.button_main_startStop)
+        buttonReset = findViewById(R.id.button_main_reset)
     }
 
     override fun onStart() {
